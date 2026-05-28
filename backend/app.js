715f8b38c6f167cf09a7d55078456ca59cf9a2a5
dbhavika02 <dwivedibhavika40@gmail.com>
2026-05-28 10:46:45 +0530

    testing backend

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,10 @@ var app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/test", (req, res) => {
+    res.send("Test route works");
+});
+
 app.get("/", (req, res) => {
     res.send("Wizard World API Running");
 });
