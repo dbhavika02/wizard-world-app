c7adbe95a1d6db36512e804f03b7f91c17f74c12
dbhavika02 <dwivedibhavika40@gmail.com>
2026-06-11 19:00:43 +0530

    included env file in frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://wizard-backend.purplecliff-8da96cf8.centralus.azurecontainerapps.io/api",
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default API;
