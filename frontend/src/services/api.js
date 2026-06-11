263a764edc2f87b99f971ee9de272237fb60b495
dbhavika02 <dwivedibhavika40@gmail.com>
2026-06-11 13:13:33 +0530

    updated the baseURL for frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://wizardapi.trippl-hq.in/api",
+    baseURL: "https://wizard-backend.purplecliff-8da96cf8.centralus.azurecontainerapps.io/api",
 });
 
 export default API;
